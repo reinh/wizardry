77f9fcd32664197b97b681fb44c028cc1e2b67a7
Rein Henrichs <reinh@reinh.com>
2008-06-11 15:46:39 -0400

    Adding ActiveRecord database for testing AR Spellbook

---
+++ spec/db/schema.rb
@@ -0,0 +1,7 @@
+ActiveRecord::Schema.define(:version => 0) do
+  create_table :posts, :force => true do |t|
+    t.column :title, :string
+    t.column :body, :string
+    t.column :author_id, :integer
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
 require 'init'
+require 'rubygems'
+require 'activerecord'
+
+plugin_spec_dir = File.dirname(__FILE__)
+ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/db/debug.log")
+
+databases = YAML::load(IO.read(plugin_spec_dir + "/db/database.yml"))
+
+# Specify sqlite dbfile location independently
+databases["sqlite3"][:dbfile] = plugin_spec_dir + "/db/wizardry.sqlite3.db"
+
+ActiveRecord::Base.establish_connection(databases[ENV["DB"] || "sqlite3"])
+load(File.join(plugin_spec_dir, "db", "schema.rb"))
